0d96749b3486203aabd7ff02fec3d4a9293545f8
Sam Odom <samodom@me.com>
2014-12-23 17:53:21 -0600

    Initial Commit

---
+++ TestableFoundation/TestableFoundation.h
@@ -0,0 +1,19 @@
+//
+//  TestableFoundation.h
+//  TestableFoundation
+//
+//  Created by Sam Odom on 12/23/14.
+//  Copyright (c) 2014 Swagger Soft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TestableFoundation.
+FOUNDATION_EXPORT double TestableFoundationVersionNumber;
+
+//! Project version string for TestableFoundation.
+FOUNDATION_EXPORT const unsigned char TestableFoundationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestableFoundation/PublicHeader.h>
+
+
